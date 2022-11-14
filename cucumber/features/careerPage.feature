cd6ce2a63cb41a4f6df90fb16da53e285cbc5649
Balazs Szabo <balazs_szabo1@epam.com>
2022-11-14 16:18:56 +0100

    UI testing with cucumber

--- cucumber/features/careerPage.feature
+++ cucumber/features/careerPage.feature
@@ -2,3 +2,29 @@ Feature: EPAM job searching
   As a Job searcher
   I want to browser through EPAM Job offers by various criteria
   So I can find to best fitting offer for me
+
+  Scenario Outline: Searching for a job - <PositionName>
+    Given the career page is opened
+    Then the logo should be visible
+    And the search form should be visible
+
+    When <location> is selected in the location filter box
+    Then <location> should be selected in the location filter box
+
+    When <skills> is selected in the skills filter box
+    Then <skills> should be selected in skills filter box
+
+    When the search button is clicked on
+    Then there should be a job offer for <PositionName> position
+    And the location of the <PositionName> position should be <location>
+    And the apply button of the <PositionName> position should be visible
+
+    When the apply button of the <PositionName> position is clicked on
+    Then the description of the job offer should contain <PositionName>
+    And there should be an apply button
+
+
+    Examples:
+            | location     | skills                                 | PositionName              |
+            | Debrecen     | Software, System, and Test Engineering | Test Automation Engineer  |
+            | Paris        | Solution Architecture                  | Solution Architect        |
--- cucumber/step_definitions/careerPage.js
+++ cucumber/step_definitions/careerPage.js
@@ -1,5 +1,67 @@
 'use strict';
 
+const { expect } = require('chai');
 const { Given, When, Then, setDefaultTimeout } = require('cucumber');
+const CareerPage = require('../../pageObjects/careerPage_cucumber');
+const careerPage = new CareerPage();
 
 setDefaultTimeout(GLOBAL_TIMEOUT);
+
+Given(/the career page is opened/, () => {
+    return careerPage.load();
+});
+careerPage.acceptCookies();
+When(/(.+) is selected in the location filter box/, (location) => {
+    return careerPage.selectLocation(location);
+});
+
+When(/(.+) is selected in the skills filter box/, (skills) => {
+    return careerPage.toggleSkills(skills);
+});
+
+When(/the search button is clicked on/, () => {
+    return careerPage.search()
+});
+
+When(/the apply button of the (.+) position is clicked on/, (PositionName) => {
+    PositionName = careerPage.getFirstResultByPosition(PositionName);
+    return careerPage.applyForPosition(PositionName);
+});
+
+Then(/the logo should be visible/, () => {
+    return expect(careerPage.logo.isDisplayed()).to.eventually.be.true;
+});
+
+Then(/the search form should be visible/, () => {
+    return expect(careerPage.searchForm.isDisplayed()).to.eventually.be.true;
+});
+
+Then(/(.+) should be selected in the location filter box/, (location) => {
+    return expect(careerPage.getSelectedLocation(location)).to.eventually.equal(location);
+});
+
+Then(/(.+) should be selected in skills filter box/, (skills) => {
+    return expect(careerPage.selectedSkills.getText()).to.eventually.contain(skills.toUpperCase());
+});
+
+Then(/there should be a job offer for (.+) position/, (PositionName) => {
+    return expect(careerPage.getFirstResultByPosition(PositionName).isDisplayed()).to.eventually.be.true;
+})
+
+Then(/the location of the (.+) position should be (.+)/, (PositionName, location) => {
+    PositionName = careerPage.getFirstResultByPosition(PositionName);
+    return expect(careerPage.locationOfPosition(PositionName).getText()).to.eventually.contain(location.toUpperCase());
+});
+
+Then(/the apply button of the (.+) position should be visible/, PositionName => {
+    PositionName = careerPage.getFirstResultByPosition(PositionName);
+    return expect(careerPage.applyLinkOfPosition(PositionName).isDisplayed()).to.eventually.be.true;
+});
+
+Then(/the description of the job offer should contain (.+)/, PositionName => {
+    return expect(careerPage.jobDescription.getText()).to.eventually.contain(PositionName);
+});
+
+Then(/there should be an apply button/,() =>{
+    return expect(careerPage.applyForJob.isDisplayed()).to.eventually.be.true;
+} )
